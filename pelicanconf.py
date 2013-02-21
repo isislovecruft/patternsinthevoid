0ca8c30d72e0ad41ccf36989a7794644cfde3a7b
Isis Lovecruft <isis@torproject.org>
2013-02-21 19:26:56 +0000

    Add changes to pelican config file.

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,21 +11,29 @@ AUTHOR = u'Isis Agora Lovecruft'
 SITENAME = u'Patterns in the Void'
 SITEURL = 'https://blog.patternsinthevoid.net'
 GITHUB_URL = 'https://github.com/isislovecruft'
-#ARTICLE_URL = '{ SITEURL }/{ slug }'
-DEFAULT_CATEGORY = u'/dev/random' # if no "Category: " in post, use this
+TWITTER_USERNAME = "isislovecruft"
+#ARTICLE_URL = SITEURL'/{ slug }'
+DEFAULT_CATEGORY = u'hacking' # if no "Category: " in post, use this
 USE_FOLDER_AS_CATEGORY = True
+NEWEST_FIRST_ARCHIVES = True
+DISQUS_SITENAME = "patternsinthevoid"
+#MENU_ITEMS = [
+#    ('Hacking', os.path.join(SITENAME, 'category/hacking.html')),
+#    ('Anarchism', os.path.join(SITENAME, 'category/anarchism.html')),
+#    ('Physics', os.path.join(SITENAME, 'category/physics.html')),
+#    ('Wandering', os.path.join(SITENAME, 'category/wandering.html')),
+#    ]
 
 # Dev settings:
 DELETE_OUTPUT_DIRECTORY = True
 OUTPUT_PATH = 'published'
 
 # Theme settings:
-THEME = 'newspaper-theme'
+THEME = 'gazette'
 THEME_STATIC_PATHS = ['static', 'images', 'prettify']
 CSS_FILE = 'main.css' 
 TYPOGRIFY = True
 PRETTIFY = True
-PRETTIFY_CSS_FILE = 'prettify.css'
 
 # Timezones, language, and metadata:
 TIMEZONE = 'UTC'
@@ -35,7 +43,7 @@ DEFAULT_DATE_FORMAT = '%A, %d %B %Y'
 # On Unix/Linux
 DATE_FORMATS = {'en': ('en_US','%A, %d %B %Y'),}
 
-FEED_DOMAIN = SITEURL
+#FEED_DOMAIN = SITEURL
 FEED_RSS = u'feed.rss.xml'
 FEED_RSS_ALL = u'all.rss.xml'
 FEED_ATOM = u'feed.atom.xml'
@@ -45,27 +53,26 @@ CATEGORY_FEED_RSS = None
 FEED_MAX_ITEMS = 15
 
 TAG_CLOUD_STEPS = 10
-TAG_CLOUD_MAX_ITEMS = 100
+TAG_CLOUD_MAX_ITEMS = 10
 
 # min articles on last page:
 DEFAULT_ORPHANS = 2
 # max per page (not including orphans):
 DEFAULT_PAGINATION = 3
-SUMMARY_MAX_LENGTH = 50
+SUMMARY_MAX_LENGTH = 250
 
 # Blogroll
 LINKS =  (
-    ('Home', 'https://patternsinthevoid.net'),
-    ('Crypto', 'https://patternsinthevoid.net/isis'),
-    ('Code', 'https://code.patternsinthevoid.net'),
-    ('Image', 'https://image.patternsinthevoid.net'),
-    ('Sound', 'https://sound.patternsinthevoid.net'),
-    ('Jinja2', 'http://jinja.pocoo.org'),
+    ('main', 'https://patternsinthevoid.net'),
+    ('crypto', 'https://patternsinthevoid.net/isis.html'),
+    ('contact', 'https://blog.patternsinthevoid.net/about.html'),
+    ('projects', 'https://git.patternsinthevoid.net'),
     )
+#    ('Image', 'https://image.patternsinthevoid.net'),
+#    ('Sound', 'https://sound.patternsinthevoid.net'),
 
 # Social widget
 SOCIAL = (
     ('Twitter', 'https://twitter.com/#!/isislovecruft'),
     ('Github', 'https://github.com/isislovecruft'),
-    ('Another social link', '#'),
     )
